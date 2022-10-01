d6b504bdba69b4636cd9605eb0f4ed600d9fac29
Pontus Lurcock <pont@talvi.net>
2022-10-01 20:01:23 +0200

    Initial commit

---
+++ firemarks.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Pontus Lurcock
+
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""List bookmarks on the Firefox bookmarks toolbar as org-mode links"""
+
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import List
+import os
+import shutil
+import tempfile
+import configparser
+import sqlite3
+import argparse
+import subprocess
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--clipboard",
+        "-c",
+        action="store_true",
+        help="write output to X clipboard, not standard output",
+    )
+    args = parser.parse_args()
+    db_path = os.path.join(expand_path("~/.mozilla/firefox"),
+                           get_default_moz_profile(),
+                           "places.sqlite")
+    bookmarks = get_toolbar_bookmarks(db_path)
+    if args.clipboard:
+        subprocess.run(
+            # "-loops 2" is specified because in practice (at least on my
+            # system) something seems to read the clipboard as soon as it's
+            # updated, so "-loops 2" is required to keep xclip running until
+            # the user pastes from the clipboard.
+        ["xclip", "-target", "UTF8_STRING", "-in", "-verbose",
+         "-selection", "clipboard", "-loops", "2"],
+            check=True,
+            encoding="utf-8",
+            input="".join(map(lambda b: b.to_org() + "\n", bookmarks))
+        )
+    else:
+        for bookmark in bookmarks:
+            print(bookmark.to_org())
+        
+
+def get_toolbar_bookmarks(db_path: str) -> List[Bookmark]:
+    with tempfile.TemporaryDirectory() as tempdir:
+        db_temp_path = os.path.join(tempdir, "places.sqlite")
+        # Firefox locks the database so we work from a copy
+        shutil.copy2(db_path, db_temp_path)
+        db = sqlite3.connect("file:" + db_temp_path + "?mode=ro")
+        cursor = db.cursor()
+        cursor.execute("SELECT id FROM moz_bookmarks WHERE title='toolbar'")
+        toolbar_id = list(cursor)[0][0]
+        cursor.execute(
+            f"SELECT moz_places.url, moz_bookmarks.title "
+            f"FROM moz_places "
+            f"INNER JOIN moz_bookmarks ON moz_places.id=moz_bookmarks.fk "
+            f"WHERE moz_bookmarks.parent={toolbar_id}"
+        )
+        return [Bookmark(url, title) for url, title in cursor]
+
+
+def get_default_moz_profile():
+    """Get the directory name of the default Firefox profile
+
+    Works under Ubuntu 20.04 for standard Firefox from Ubuntu repos.
+    Not guaranteed for other Firefox installs."""
+    ini_path = expand_path("~/.mozilla/firefox/profiles.ini")
+    config_parser = configparser.ConfigParser()
+    config_parser.read(ini_path)
+    for section in config_parser.sections():
+        config_dict = config_parser[section]
+        if "Name" in config_dict and config_dict["Name"] == "default-release":
+            print(config_dict["path"])
+            return config_dict["path"]
+    return None
+
+        
+def expand_path(path):
+    return os.path.expandvars(os.path.expanduser(path))
+
+
+@dataclass
+class Bookmark:
+    url: str
+    title: str
+
+    def to_org(self):
+        return f"[[{self.url}][{self.title}]]"
+
+        
+if __name__ == "__main__":
+    main()
