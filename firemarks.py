4d034854636abfd0543d9bf10bb8942e60da7584
Pontus Lurcock <pont@talvi.net>
2025-03-08 12:43:48 +0100

    Update copyright years

--- firemarks.py
+++ firemarks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2022–2024 Pontus Lurcock
+# Copyright (c) 2022–2025 Pontus Lurcock
 
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
